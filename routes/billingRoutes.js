3a6c4822c9be829b7971259b2a390d2d1e750901
Chiciuc Nicușor <nicuchiciuc@gmail.com>
2018-09-23 17:28:28 +0300

    Finished billing

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,20 +1,19 @@
 const keys = require('../config/keys')
 const stripe = require('stripe')(keys.stripeSecretKey)
+const requireLogin = require('../middlewares/requireLogin')
 
 module.exports = app => {
-  app.post('/api/stripe', (req, res) => {
-    console.log(req.body)
+  app.post('/api/stripe', requireLogin, async (req, res) => {
+    const charge = await stripe.charges.create({
+      amount: 500,
+      currency: 'usd',
+      description: '$5 for 5 credits',
+      source: req.body.id,
+    })
+
+    req.user.credits += 5
+    const user = await req.user.save()
+
+    res.send(user)
   })
 }
-
-// stripe.charges.create(
-//   {
-//     amount: 2000,
-//     currency: 'usd',
-//     source: 'tok_visa', // obtained with Stripe.js
-//     description: 'Charge for jenny.rosen@example.com',
-//   },
-//   function(err, charge) {
-//     // asynchronously called
-//   }
-// )
